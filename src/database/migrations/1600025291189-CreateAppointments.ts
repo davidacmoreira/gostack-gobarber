e45b85d46d2650036e9de55e5c4c22c429f009cc
David Moreira <davidmoreira37@gmail.com>
2020-09-14 08:43:43 +0100

    Add Appointments migration and refactor code to get and save appointments to db

--- src/database/migrations/1600025291189-CreateAppointments.ts
+++ src/database/migrations/1600025291189-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1600025291189
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
