2ecf3dbbd85309108574d4a5e694fd1699705b82
David Moreira <davidmoreira37@gmail.com>
2020-11-10 18:35:52 +0000

    Add Ethereal mail provider for test and dev environment

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -30,9 +30,12 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(email, 'Request to recover password received');
+    await this.mailProvider.sendMail(
+      email,
+      `Request to recover password received ${token}`,
+    );
   }
 }
 
