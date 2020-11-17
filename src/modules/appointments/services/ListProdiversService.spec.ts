b707ef26a19bb4f92d131e20007eaf11d2482864
David Moreira <davidmoreira37@gmail.com>
2020-11-17 10:36:53 +0000

    Add list providers

---
+++ src/modules/appointments/services/ListProdiversService.spec.ts
@@ -0,0 +1,38 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProdiversService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProvidersService: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    listProvidersService = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'Joe',
+      email: 'joe@example.com',
+      password: '123456',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'Tom',
+      email: 'Tom@example.com',
+      password: '123456',
+    });
+
+    const providers = await listProvidersService.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([user1, user2]);
+  });
+});
