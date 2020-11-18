6fdafeeeeb19053df77fa6707aa713db32c37ead
David Moreira <davidmoreira37@gmail.com>
2020-11-18 09:31:08 +0000

    Add celebrate and validate routes data

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,14 +1,29 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import UsersProfileController from '../controllers/UsersProfileController';
 
 const usersProfileRouter = Router();
+
 const usersProfileController = new UsersProfileController();
 
 usersProfileRouter.use(ensureAuthenticated);
 
 usersProfileRouter.get('/', usersProfileController.show);
-usersProfileRouter.put('/', usersProfileController.update);
+
+usersProfileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      previous_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  usersProfileController.update,
+);
 
 export default usersProfileRouter;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 
@@ -8,11 +9,23 @@ import UsersController from '../controllers/UsersController';
 import UsersAvatarController from '../controllers/UsersAvatarController';
 
 const usersRouter = Router();
+
 const usersController = new UsersController();
 const usersAvatarController = new UsersAvatarController();
+
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
